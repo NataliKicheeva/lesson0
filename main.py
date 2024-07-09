e85ab50c92fbae264ff08f0343afb024600cc450
NataliKicheeva <tusya21po@gmail.com>
2024-07-09 23:00:53 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # "1st program"
-print('9**0.5*5=15.0')
 print(9 ** 0.5 * 5)
 
 # "2st program"
-print('99.99>9.98, 1000!=1000.1')
 print(9.99 > 9.98 and 1000 != 1000.1)
 
 # "3rd program"
 print((1234 % 1000 // 10) + (5678 % 1000 // 10))
 
-# "4ht program
-print(13.00 == 13.42 or 42.13)
+# "4ht program)
+print(13 == (int(13.42) or (42.13 - 42)))
+print(42 == (int(42.13) or (13.42 - 13)))

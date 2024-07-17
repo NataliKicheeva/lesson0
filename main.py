f1f553feb6ac87dbb9277b8bffb05cb3551ab97a
NataliKicheeva <tusya21po@gmail.com>
2024-07-17 17:04:08 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ print(9 ** 0.5 * 5)
 print(9.99 > 9.98 and 1000 != 1000.1)
 
 # "3rd program"
-print((1234 % 1000 // 10) + (5678 % 1000 // 10))
+print((1234 % 2000 // 10) + (5678 % 1000 // 10))
 
 # "4ht program)
-print(13 == (int(13.42) or (42.13 - 42)))
-print(42 == (int(42.13) or (13.42 - 13)))
+num1 = 13.42
+num2 = 42.13
+int_part1 = int(num1)
+int_part2 = int(num2)
+frac_part1 = int((num1 - int_part1) * 100)
+frac_part2 = int((num2 - int_part2) * 100)
+result = (int_part1 == frac_part2) or (int_part2 == frac_part1)
+print(result)
